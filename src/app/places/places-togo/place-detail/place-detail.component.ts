8e3c034d5f10d535c2029180fc3803d246e728f7
Radek F <radek.fr@email.cz>
2018-11-20 21:32:33 +0100

    adding bing maps to place detail

--- src/app/places/places-togo/place-detail/place-detail.component.ts
+++ src/app/places/places-togo/place-detail/place-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+/// <reference path="../../../../../node_modules/bingmaps/types/MicrosoftMaps/Microsoft.Maps.All.d.ts" />
+
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { Place } from '../../place.model';
 import { ActivatedRoute, Params } from '@angular/router';
 import { PlacesService } from '../../places.service';
@@ -11,6 +13,7 @@ import { PlacesService } from '../../places.service';
 export class PlaceDetailComponent implements OnInit {
   place: Place
   index: number;
+  @ViewChild("myMap") myMap
 
   constructor(private route: ActivatedRoute,
               private placesService: PlacesService) { }
@@ -20,6 +23,10 @@ export class PlaceDetailComponent implements OnInit {
       this.index = params["id"]
       this.place = this.placesService.getPlace(this.index)
     })
+    const map = new Microsoft.Maps.Map(this.myMap.nativeElement, {
+      credentials: "Asdww3lvu-L5BZ57ZGqInk6fqN_8A6BBIWYKPl67rkTJb8PlQOJTkyYv6KrnFRwd"
+    })
   }
+
 
 }
