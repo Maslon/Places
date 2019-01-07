971da86953cd8640a887f8b76c9629ed2f51c5bb
Radek F <radek.fr@email.cz>
2019-01-08 00:08:17 +0100

    adding map basics, comments

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { tileLayer, latLng } from 'leaflet';
+// import "leaflet-search"
+// import * as leafletSearch from "leaflet-search"
+// import * as L from 'leaflet'
+declare let L
+import "leaflet-search"
+
+
 
 @Component({
   selector: 'app-map',
@@ -7,20 +14,51 @@ import { tileLayer, latLng } from 'leaflet';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-  options = {
-    layers: [
-      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; OpenStreetMap contributors'
-      })
-    ],
-    zoom: 7,
-    center: latLng([ 46.879966, -121.726909 ])
-  };
+  // searchLayer
+  
 
+  // options = {
+  //   layers: [
+  //     tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  //       attribution: '&copy; OpenStreetMap contributors'
+  //     })
+  //   ],
+  //   zoom: 7,
+  //   center: latLng([ 46.879966, -121.726909 ])
+  // };
 
   constructor() { }
 
   ngOnInit() {
+    const map = L.map('map').setView([51.505, -0.09], 13)
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+    // const searchLayer = L.layerGroup().addTo(map);
+    // map.addControl( new L.Control.Search({layer: searchLayer}) );
+    L.control.scale().addTo(map);
+
+// Create a Tile Layer and add it to the map
+//var tiles = new L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png').addTo(map);
+  var searchControl = new L.esri.Controls.Geosearch().addTo(map);
+
+  var results = new L.LayerGroup().addTo(map);
+
+  searchControl.on('results', function(data){
+    results.clearLayers();
+    for (var i = data.results.length - 1; i >= 0; i--) {
+      results.addLayer(L.marker(data.results[i].latlng));
+    }
+  });
+
   }
 
+  // onMapReady(map: L.Map){
+  //   L.Control.Search
+  // }
+
+  // onMapReady(map: L.Map) {
+  //   L.control.coordinates({}).addTo(map);
+  // }
 }
