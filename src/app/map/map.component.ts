1784874015b25aca9c1131f150b4a1e0932418ca
Radek F <radek.fr@email.cz>
2019-01-08 22:08:30 +0100

    cleaning map component

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,14 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-// import "leaflet-search"
-// import * as leafletSearch from "leaflet-search"
-// import * as L from 'leaflet'
 import "leaflet"
 declare let L
-// import * as L from 'leaflet'
-import "leaflet-search"
-// import "esri-leaflet"
-import * as esri from "esri-leaflet"
-// import "esri-leaflet-geocoder"
 import * as esrigeo from "esri-leaflet-geocoder"
 
 
@@ -20,57 +12,28 @@ import * as esrigeo from "esri-leaflet-geocoder"
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-  // searchLayer
-
-  // options = {
-  //   layers: [
-  //     tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  //       attribution: '&copy; OpenStreetMap contributors'
-  //     })
-  //   ],
-  //   zoom: 7,
-  //   center: latLng([ 46.879966, -121.726909 ])
-  // };
+  map
 
   constructor() { }
 
   ngOnInit() {
-    console.log(L)
-    console.log(esri)
-    console.log(esrigeo)
-
-    // console.log(esri)
-    // console.log(esrigeo)
-
-    let map = L.map('map').setView([51.505, -0.09], 13)
+    this.map = L.map('map').setView([51.505, -0.09], 13)
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
-    // const searchLayer = L.layerGroup().addTo(map);
-    // map.addControl( new L.Control.Search({layer: searchLayer}) );
-    L.control.scale().addTo(map);
+        }).addTo(this.map);
+    L.control.scale().addTo(this.map);
+    let searchControl = new esrigeo.Geosearch().addTo(this.map);
 
-// Create a Tile Layer and add it to the map
-//var tiles = new L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png').addTo(map);
-  let searchControl = new esrigeo.Geosearch().addTo(map);
-
-  const results = new L.LayerGroup().addTo(map);
-  searchControl.on('results', function(data){
-    results.clearLayers();
-    for (let i = data.results.length - 1; i >= 0; i--) {
-      results.addLayer(L.marker(data.results[i].latlng));
-    }
-  });
-  console.log(esri)
+    const results = new L.LayerGroup().addTo(this.map);
+    searchControl.on('results', function(data){
+      console.log(data)
+      results.clearLayers();
+      for (let i = data.results.length - 1; i >= 0; i--) {
+        results.addLayer(L.marker(data.results[i].latlng));
+      }
+    });
 
   }
 
-  // onMapReady(map: L.Map){
-  //   L.Control.Search
-  // }
-
-  // onMapReady(map: L.Map) {
-  //   L.control.coordinates({}).addTo(map);
-  // }
 }
